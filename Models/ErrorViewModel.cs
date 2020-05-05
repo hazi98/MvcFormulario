d160651f439eafb61cc3a9c73c56fc81ebae2c81
Jassiel <contact@ochrefox.net>
2020-05-04 19:40:46 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcFormulario.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
